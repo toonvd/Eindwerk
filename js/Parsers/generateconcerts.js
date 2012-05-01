cac5572d8ae33743f4aaee6a23cf6d27bd57c35d
Annelies Deleu <deleu.annelies@gmail.com>
2012-05-01 15:07:42 +0200

    xmls

--- js/Parsers/generateconcerts.js
+++ js/Parsers/generateconcerts.js
@@ -33,6 +33,7 @@ $(document).ready(function generatexml(){
                 $('#concerten').append(line+'<div id="tabel" onclick="navigate('+"'"+$(this).find('artist').text()+"'"+')"><div id="tabelimage"><img src="http://griekenland.mixxt.at/storage/images/events/0/0/0/00000000000000000000000000000.jpg"/></div>'
                     +'<div id="artist">'+$(this).find('artist').text()+'</div>'
                     +'<div id="date">'+$(this).find('date').text()+'</div>'
+					+'<div id="location">'+$(this).find('location').text()+'</div>'
                     +'<div id="review">'+$(this).find('review').text()+'</div>').trigger('create');
                 counter ++;
             })
