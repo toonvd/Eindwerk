8de71af0bce1a84915453ac963792528fd27364a
Annelies Deleu <deleu.annelies@gmail.com>
2012-05-20 15:07:18 +0200

    free stuff

--- js/Parsers/generatexml.js
+++ js/Parsers/generatexml.js
@@ -16,7 +16,7 @@ function generatexml(dateText){
             $(xml).find("concert").filter(function(){
                 return $(this).find("date").text().indexOf(dateText) === 0;
             }).each(function(){
-                $('#container').append('<div id="tabel" onclick="navigate('+"'"+$(this).find('artist').text()+"'"+')"><div id="tabelimage"><img src="http://griekenland.mixxt.at/storage/images/events/0/0/0/00000000000000000000000000000.jpg"/></div>'
+                $('#container').append('<div id="tabel" onclick="navigate('+"'"+$(this).find('artist').text()+"'"+')"><div id="tabelimage"><img src="'+$(this).find('image').text()+'"/></div>'
                     +'<div id="tabelartist">'+$(this).find('artist').text()+'</div>'
                     +'<div id="tabellocation">'+$(this).find('location').text()+'</div>'
                     ).trigger('create');
