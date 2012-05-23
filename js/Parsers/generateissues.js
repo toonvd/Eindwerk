990f49d2a27a3595091e6c64ac286eefcb3dd5b0
Toon Van Dooren <toon_vd@live.com>
2012-05-23 14:56:23 +0200

    test

--- js/Parsers/generateissues.js
+++ js/Parsers/generateissues.js
@@ -8,7 +8,7 @@ function getNode(id) {
                     $('#issues').empty(); 
                         $('#issues').append('<div id="block"><div id="title"><strong>'+oIssue.find('title').text()+'</strong></div>'
                     +'<div id="date">'+oIssue.find('date').text()+'</div><div id=""><img src="'+oIssue.find('image').text()+'"alt="articleimage"/></div>'
-                    +'<div id="content"><a href="">deze maand</a></div></div><br />').trigger('create');
+                    +'<div id="content"><a href="" onclick="window.location.href='+"'"+'download.php?f='+oIssue.find('file').text()+"'"+'">deze maand</a></div></div><br />').trigger('create');
                     }
                 })
             }
