69def970de8abb4e1fa1833f83d4fb88629eb375
Annelies Deleu <deleu.annelies@gmail.com>
2012-05-23 20:37:31 +0200

    freestuff + issues + kalender

--- js/Parsers/generateissues.js
+++ js/Parsers/generateissues.js
@@ -6,9 +6,10 @@ function getNode(id) {
                     success: function(xml){
                         oIssue = $(xml).find('issue:eq('+id+')');
                     $('#issues').empty(); 
-                        $('#issues').append('<div id="block"><div id="title"><strong>'+oIssue.find('title').text()+'</strong></div>'
-                    +'<div id="date">'+oIssue.find('date').text()+'</div><div id=""><img src="'+oIssue.find('image').text()+'"alt="articleimage"/></div>'
-                    +'<div id="content"><a href="" onclick="window.location.href='+"'"+'download.php?f='+oIssue.find('file').text()+"'"+'">deze maand</a></div></div><br />').trigger('create');
+                        $('#issues').append('<div id="block"><div id="titleissue"><strong>'+oIssue.find('title').text()+'</strong></div>'
+						  	+'<div id="issue_date">'+oIssue.find('date').text()+'</div>'
+                   			+'<div id=""><img src="'+oIssue.find('image').text()+'"alt="coverimage" id="coverimage"/></div>'
+				 			+'<div id="content"><a href="" id="downloadbtn" onclick="window.location.href='+"'"+'download.php?f='+oIssue.find('file').text()+"'"+'">Download</a></div></div><br />').trigger('create');
                     $('#uitleg').empty(); 
                     $('#uitleg').append(oIssue.find('text').text()).trigger('create');
                 }
